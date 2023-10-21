11c459a0bf25eba45b9915fdff9ba0db1d46d972
J Parker Tonra <parkertonra@gmail.com>
2023-10-21 17:39:22 -0600

    fixed routes.py to work with iframe embed

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
-from flask import render_template, redirect, url_for, request, flash, send_from_directory
+from flask import render_template, redirect, url_for, request, flash, send_from_directory, make_response
 from app import app
 import os
+
+@app.after_request
+def apply_frame_options(response):
+    response.headers["X-Frame-Options"] = "ALLOWALL"
+    return response
 
 @app.route('/')
 @app.route('/index')
